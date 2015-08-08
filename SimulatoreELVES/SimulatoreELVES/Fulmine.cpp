b2a776df47a61e00e2cbfc50b574132e04607ea4
Kaerios89 <andrea.balistreri@gmail.com>
2015-08-08 17:27:36 +0200

    incusione di un file escluso dal commit precedente

---
+++ SimulatoreELVES/SimulatoreELVES/Fulmine.cpp
@@ -0,0 +1,16 @@
+/*DA DECIDERE: non so se conviene costruire una classe per il fulmine o definire una sruct nel main.
+Qui vengono conservate le informazioni di posizione e potenza del fulmine. Tali informazioni sono accessibili da tutti i pixel
+e deve essere garantita l'accessibilità (in lettura) in parallelo. Ovviamente la scrittura è mutualmente esclusiva, ma una volta
+che il fulmine è stato dichiarato non dovrebbe essere più modificato.*/
+#include "stdafx.h"
+#include "Fulmine.h"
+
+
+Fulmine::Fulmine()
+{
+}
+
+
+Fulmine::~Fulmine()
+{
+}
