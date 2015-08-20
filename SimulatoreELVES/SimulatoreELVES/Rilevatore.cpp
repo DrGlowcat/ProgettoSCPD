9c72930d3420343c108490091359e6364f2d8529
PrimaevalWorld <PrimaevalWorld@users.noreply.github.com>
2015-08-20 11:43:03 +0200

    implementazione GetOrientation - work in progress

--- SimulatoreELVES/SimulatoreELVES/Rilevatore.cpp
+++ SimulatoreELVES/SimulatoreELVES/Rilevatore.cpp
@@ -1,6 +1,6 @@
-/*Similmente alla classe Ionosfera, Rilevatore è un contenitore di Pixel. Anche qui è possibile definire la "risoluzione" del rilevatore
-ovvero il numero di pixel del rilevatore stesso. Diversamente dalla classe Ionosfera possiamo avere più oggetti di tipo Rilevatore presenti
-contemporaneamente nel sistema. Ogni pixel del rilevatore è associato a uno o più IonPixel, va quindi implementata una funzione che, 
+/*Similmente alla classe Ionosfera, Rilevatore Ã¨ un contenitore di Pixel. Anche qui Ã¨ possibile definire la "risoluzione" del rilevatore
+ovvero il numero di pixel del rilevatore stesso. Diversamente dalla classe Ionosfera possiamo avere piÃ¹ oggetti di tipo Rilevatore presenti
+contemporaneamente nel sistema. Ogni pixel del rilevatore Ã¨ associato a uno o piÃ¹ IonPixel, va quindi implementata una funzione che, 
 potenzialmente, possa sommare i vari contributi.*/
 #include "stdafx.h"
 #include "Rilevatore.h"
@@ -27,7 +27,13 @@ bool Rilevatore::GetStatus()
 }
 double Rilevatore::GetOrientation()
 {
-
+   /* Ã¨ ancora una bozza: per la tesi avevo fatto il calcolo usando vettori 3D,
+	 * che sono perÃ² una classe di ROOT. devo capire come farlo con gli strumenti del C++
+	 */
+	double degree = 3.14159265359/180.  //in realtÃ  conviene fare un file .h con tutte le costanti
+	double SiteLocation[2];  //coordinate del sito cui appartiene il rilevatore
+	double Phi = LatSite*degree;  //coord espresse in radianti
+	double Theta = LongSite*degree;
 }
 double Rilevatore::GetX()
 {
