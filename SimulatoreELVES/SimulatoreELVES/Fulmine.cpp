a121f30a696a5ca392b4dccfc2ef6f71073c2701
PrimaevalWorld <PrimaevalWorld@users.noreply.github.com>
2015-08-20 10:42:18 +0200

    Update Fulmine.cpp
    ho riempito le funzioni e creato la funzione Set, che genera le caratteristiche del fulmine; bisogna decidere un random generator adatto al parallelismo, credo. Ho tolto z perchè l'altezza da cui parte il fulmine è circa sempre la stessa, è importante solo nell'ELVES.
    PS: per adesso sto lavorando via web perché non sono riuscita a configurare eclipse per git, ho solo perso un sacco di tempo.

--- SimulatoreELVES/SimulatoreELVES/Fulmine.cpp
+++ SimulatoreELVES/SimulatoreELVES/Fulmine.cpp
@@ -1,13 +1,14 @@
 /*DA DECIDERE: non so se conviene costruire una classe per il fulmine o definire una sruct nel main.
 Qui vengono conservate le informazioni di posizione e potenza del fulmine. Tali informazioni sono accessibili da tutti i pixel
-e deve essere garantita l'accessibilità (in lettura) in parallelo. Ovviamente la scrittura è mutualmente esclusiva, ma una volta
-che il fulmine è stato dichiarato non dovrebbe essere più modificato.*/
+e deve essere garantita l'accessibilitÃ  (in lettura) in parallelo. Ovviamente la scrittura Ã¨ mutualmente esclusiva, ma una volta
+che il fulmine Ã¨ stato dichiarato non dovrebbe essere piÃ¹ modificato.*/
 #include "stdafx.h"
 #include "Fulmine.h"
 
 
 Fulmine::Fulmine()
 {
+  SetBolt(Tempo,Lat,Long,Energy);
 }
 
 
@@ -15,22 +16,32 @@ Fulmine::~Fulmine()
 {
 }
 
+void SetBolt(double &Tempo, double &Lat, double &Long, double &Energy)
+{
+	//Tempo=some rand();  misurato in microsec
+	//Lat=some rand();   in gradi da Greenwich; range: [-41.5828;-25.25]
+	//Long=some rand();   range: [-79.25;-59.25]
+	//Energy=some rand();   in joule. forse non serve
+}
+
+
 double Fulmine::GetTempo()
 {
+	return Tempo;
 }
 
-double Fulmine::GetX()
+//il fulmine ha solo latitudine e longitudine
+double Fulmine::GetLat()
 {
+	return Lat;
 }
 
-double Fulmine::GetY()
+double Fulmine::GetLong()
 {
-}
-
-double Fulmine::GetZ()
-{
+	return Long;
 }
 
 double Fulmine::GetEnergy()
 {
+	return Energy;
 }
