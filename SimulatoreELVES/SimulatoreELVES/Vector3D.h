723bddd99887a43966d980a5f17c03194f228b5e
PrimaevalWorld <PrimaevalWorld@users.noreply.github.com>
2015-08-26 19:46:02 +0200

    header della classe vector3D
    PS riesci a vedere i file che creo o devo includere stdafx?

---
+++ SimulatoreELVES/SimulatoreELVES/Vector3D.h
@@ -0,0 +1,32 @@
+/*
+ * vector3D.h
+ *
+ *  Created on: 26 Aug 2015
+ *      Author: grumpycat
+ */
+#pragma once
+#ifndef VECTOR3D_H_
+#define VECTOR3D_H_
+
+class Vector3D
+{
+//algebra vettoriale; supponiamo che i vettori 3D siano del tipo v={x,y,z}
+private:
+	double comp_x;
+	double comp_y;
+	double comp_z;
+public:
+	Vector3D();
+	~Vector3D();
+	double GetX();
+	double GetY();
+	double GetZ();
+	double Norma();  //calcola norma del vettore dato un puntatore al vettore
+	double Norma2();  //calcola norma al quadrato del vettore
+	double Dot(Vector3D *);  //prodotto scalare tra due vettori
+	Vector3D Cross(Vector3D *);  //prodotto vettoriale tra due vettori --> restituisce un vettore, non so se il prototipo è giusto così
+	double GetTheta();  //restituisce angolo polare, devo capire in quale sistema di riferimento conviene mettersi
+	double GetPhi();  //restituisce azimut
+	double Angle(Vector3D *);  //angolo tra due vettori
+};
+#endif /* VECTOR3D_H_ */
