48b9b7c0bb34e584524c4d00ad5d4fea524c89b0
PrimaevalWorld <PrimaevalWorld@users.noreply.github.com>
2015-09-03 11:13:49 +0200

    WIP sul collegamento rivelatore-ionosfera

--- SimulatoreELVES/SimulatoreELVES/Rilevatore.h
+++ SimulatoreELVES/SimulatoreELVES/Rilevatore.h
@@ -5,25 +5,34 @@ using namespace std;
 class Rilevatore
 {
 private:
+	//se la posizione è identificata da latitudine e longitudine, x,y,z sono inutili.
+	/*double PosX;  //si riferiscono alla posizione del rilevatore una sorta di media della posizione dei singoli pixel
+	double PosY;
+	double PosZ;*/
 	//AGGIUNGERE la porzione propria i atmosfera
 	//gli va dato in pasto un insieme di riferimenti globali
 	double LatSite;  //latitudine del telescopio cui appartiene il rilevatore
 	double LongSite;
 //4 telescopi --> 4 gruppi di 6 rilevatori e 4 posizioni diverse
 	double Orientation;
-	double FoV;  //Field of View: campo visivo del rivelatore, in gradi, centrato sull'Orientation
+	double right_end;  //limite destro del fov, in radianti
+	double left_end;  //limite sinistro del fov, in radianti
 	bool Status; //indica se acceso o spento.
 	int Resolution;
 	map<int, RelPixel> Matrice_Osservazione;
 public:
 	Rilevatore();
-	Rilevatore(double Lat, double Long, double fOv, int res);
+	Rilevatore(double Lat, double Long, double r_e, int res);
 	~Rilevatore();
-	map<int, RelPixel> Rel2Ion(double In_X, double In_Y, double In_Z);
+	map<int, RelPixel> Rel2Ion(double pix_lat, double pix_long);
 	bool GetStatus();
 	double GetOrientation();
+	/*double GetX();
+	double GetY();
+	double GetZ();*/
 	double GetLatSite();
 	double GetLongSite();
 	int GetResolution();
+	bool Spotted(double alpha);
 };
 
