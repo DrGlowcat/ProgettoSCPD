893b7ff22e08fa38b864503fc48f3825da64caf3
PrimaevalWorld <PrimaevalWorld@users.noreply.github.com>
2015-09-03 10:57:10 +0200

    aggiunto funzioni

--- SimulatoreELVES/SimulatoreELVES/Vector3D.h
+++ SimulatoreELVES/SimulatoreELVES/Vector3D.h
@@ -1,5 +1,4 @@
-/*
- * vector3D.h
+ /* vector3D.h
  *
  *  Created on: 26 Aug 2015
  *      Author: grumpycat
@@ -29,9 +28,12 @@ public:
 	double Norma2();  //calcola norma al quadrato del vettore
 	double Dot(Vector3D *);  //prodotto scalare tra due vettori
 	Vector3D Cross(Vector3D *);  //prodotto vettoriale tra due vettori --> restituisce un vettore, non so se il prototipo è giusto così
+	void SetTheta(double th); //imposta angolo polare
+	void SetPhi(double ph);  //imposta angolo azimutale
 	double GetTheta();  //restituisce angolo polare, devo capire in quale sistema di riferimento conviene mettersi
 	double GetPhi();  //restituisce azimut
 	double Angle(Vector3D *);  //angolo tra due vettori
 	void SetMag(double);  //imposta la norma di un vettore al valore del double fornito
 };
 #endif /* VECTOR3D_H_ */
+
