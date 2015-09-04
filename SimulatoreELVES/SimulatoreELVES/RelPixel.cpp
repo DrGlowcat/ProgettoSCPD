acdcfb3cd37409988b8ba492a735dc83df9111f0
PrimaevalWorld <PrimaevalWorld@users.noreply.github.com>
2015-09-04 17:42:10 +0200

    aggiunte varie

--- SimulatoreELVES/SimulatoreELVES/RelPixel.cpp
+++ SimulatoreELVES/SimulatoreELVES/RelPixel.cpp
@@ -1,6 +1,7 @@
+
 /*Pixel del rilevatore. Oltre ad avere informazioni sulla posizione (magari condivise con tutti i pizel di uno stesso rilevatore)
 ha le "referenze" di ogni pixel atmosferico che vede in modo da sapere da chi prendere i dati. Una funzione importante dei 
-RelPixel è il calcolo dell'ordine di ricezione dei vari segnali in modo da poter creare delle immagini in funzione del tempo
+RelPixel Ã¨ il calcolo dell'ordine di ricezione dei vari segnali in modo da poter creare delle immagini in funzione del tempo
 che siano significative e soprattutto correte.*/
 #include "stdafx.h"
 #include "RelPixel.h"
@@ -52,4 +53,21 @@ double RelPixel::GetVertAngle()
 double RelPixel::GetHoriAngle()
 {
 	return HoriAngle;
+}
+void RelPixel::SetPixElev(double theta)
+{
+	pixel_elev = theta;
+}
+
+void RelPixel::SetPixAzimut(double phi)
+{
+	pixel_azimut = phi;
+}
+double RelPixel::GetPixElev()
+{
+	return pixel_elev;
+}
+double RelPixel::GetPixAzimut()
+{
+	return pixel_azimut;
 }
