4622e0a9e464e42ea0c6fb6cafcac932d9944b7b
Kaerios89 <andrea.balistreri@gmail.com>
2015-09-19 16:39:04 +0200

    Ho solo tolto il caricamento da file dei rilevatori e ionosfera

--- SimulatoreELVES/SimulatoreELVES/SimulatoreELVES.cpp
+++ SimulatoreELVES/SimulatoreELVES/SimulatoreELVES.cpp
@@ -36,53 +36,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		cout << "File ERROR: Unable to open";
 		return 1;
 	}
-
-	/*Inizializzazione dei rilevatori:
-	Anche qui utilizziamo un file contenente i valori di posizione e orientamento dei 
-	rilevatori. Contestualmente inizializziamo i pixel di ogni rilevatore rendendoli 
-	pronti per la fase successiva. Qui abbiamo la possibilità di avere due livelli di
-	esecuzione concorrente: la prima a livello di operazione, ovvero la creazione dei
-	rilevatori è indipendente dal resto; la seconda si ha dato che i rilevatori sono 
-	indipendenti l'uno con l'altro, possiamo quindi crearli in modo parallelo. In vero
-	vi sarebbe anche un terzo livello, dato che i pixel di ogni rilevatore sono indipendenti
-	e potrebbero essere creati anch'essi in modo concorrente.
-	*/
-	ifstream RelFile("DBRilevatori.txt", ios::in | ios::ate); //da modificare in caso non si chiami così
-	if (RelFile.is_open())
-	{
-		streampos size = RelFile.tellg();
-		char* RelString = new char[size];
-		RelFile.seekg(0, ios::beg);
-		RelFile.read(RelString, size);
-		RelFile.close();
-		//ora va parsificato per tirare fuori i singoli Rilevatori e metterli in una struttura appropriata.
-	}
-	else
-	{
-		cout << "File ERROR: Unable to open";
-		return 1;
-	}
-
-	/*Inizializzazione Atmosfera
-	In questa fase si inizializza l'atmosfera da file. Come sopra il grado di parallelismo
-	è estendibile ai pixel dell'atmosfera stessa. 
-	*/
-	ifstream AtmFile("DBIonosfera.txt", ios::in | ios::ate); //da modificare in caso non si chiami così
-	if (RelFile.is_open())
-	{
-		streampos size = AtmFile.tellg();
-		char* AtmString = new char[size];
-		AtmFile.seekg(0, ios::beg);
-		AtmFile.read(AtmString, size);
-		AtmFile.close();
-		//ora va parsificato per tirare fuori i valori di inizializzaione dell'atmosfera.
-	}
-	else
-	{
-		cout << "File ERROR: Unable to open";
-		return 1;
-	}
-
+	
 	/*Da qui inizia la fase di computazione e calcolo della simulazione
 
 	for each(Bolt in DBFulmine)
