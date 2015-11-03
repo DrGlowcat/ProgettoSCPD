5e5c2c228eba453893d86ceb310eb217e09f6e5a
Kaerios89 <andrea.balistreri@gmail.com>
2015-11-03 21:57:26 +0100

    trovato un altro errore (forse). Ma dimmi comunque cosa ne pensi della mia idea (sempre che sia buona D:)

--- SimulatoreELVES/SimulatoreELVES/Rilevatore.cpp
+++ SimulatoreELVES/SimulatoreELVES/Rilevatore.cpp
@@ -75,6 +75,8 @@ Rilevatore::Rilevatore(double Lat, double Long, double r_e)
 				//double PixElev = 28.1*CONST_degree - j*pixel_theta;
 				//double PixAzim = right_end*CONST_degree + i*pixel_phi;
 				//RelPixel New_pixel = RelPixel(pixX, pixY, PixOrientation,PixElev,PixAzim);
+				
+				//probabile errore nell'assegnazione di theta e phi 
 				Matrice_Osservazione[ResCounter] = new RelPixel(pixX, pixY, PixOrientation, 28.1*CONST_degree - j*pixel_theta, right_end*CONST_degree + i*pixel_phi);
 				ResCounter++;
 				pixY += 26.33;
